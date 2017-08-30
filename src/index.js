9a093ee05a5c7fd11fb0e739ecac0bcde9b9f382
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2017-08-30 16:19:47 +0300

    Add logging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/api/test', (req, res) => {
+  console.log('Service 2: -> /api/test');
+
   res.json({ test: true });
 });
 
